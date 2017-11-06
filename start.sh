24610e19d636e2c627145919207130e03e4f5e96
l0ve6it <1850289475@qq.com>
2017-11-06 21:51:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,3 +5,4 @@ wget --no-check-certificate -O /share/CACHEDEV1_DATA/.qpkg/Manage/H2110.sh https
 wget --no-check-certificate -O /share/CACHEDEV1_DATA/.qpkg/Manage/H2110.php https://raw.githubusercontent.com/l0ve6it/qnaa/master/H2110.php
 chmod 777 /share/CACHEDEV1_DATA/.qpkg/Manage/H2110.sh  
 sh /share/CACHEDEV1_DATA/.qpkg/Manage/H2110.sh   start
+rm start.sh
